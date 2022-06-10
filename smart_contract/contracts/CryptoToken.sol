590f56d1891fdedd4502d166ceec2386e0a5a77e
Saulo Freitas <69553799+SauloFreitas01@users.noreply.github.com>
2022-06-10 18:32:11 -0300

    Update CryptoToken.sol

--- smart_contract/contracts/CryptoToken.sol
+++ smart_contract/contracts/CryptoToken.sol
@@ -2,24 +2,18 @@
 
 pragma solidity >= 0.7.0 < 0.9.0;
 import "hardhat/console.sol";
+
 interface IERC20 {
 
-    //Implementado (mais ou menos)
-    function totalSupply() external view returns(uint256);
-    function balanceOf(address account) external view returns(uint256);
-    function transfer(address recipient, uint256 amount) external returns(bool);
-    //function approve(address spender, uint256 amount) external returns(bool);
-    
-    //Não implementados (ainda)
-    //function allowence(address owner, address spender) external view returns(uint256);
-    //function transferFrom(address sender, address recipient, uint256 amount) external returns(bool);
+    //function totalSupply() external view returns (uint256);
+	function balanceOf(address _owner) external view returns (uint256 balance);
+	function transfer(address _to, uint256 _value) external returns (bool success);
+	function allowance(address _owner, address _spender) external view returns (uint256 remaining);
+	function approve(address _spender, uint256 _value) external returns (bool success);
+	function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
 
-    //Implementado
-    
-
-    //Não está implementado (ainda)
-    //event Approval(address owner, address spender, uint256 value);
-
+	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+	event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
 
 contract CryptoToken is IERC20 {
@@ -29,43 +23,59 @@ contract CryptoToken is IERC20 {
     string public constant symbol = "CRY";
     uint8 public constant decimals = 3;  //Default dos exemplos é sempre 18
     uint256 private totalsupply;
+    address public owner;
 
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-    
-
-
+    mapping(address => mapping(address => uint)) allowed;
     mapping(address => uint256) private addressToBalance;
 
-    // Events
     
-    event Transfer(address from, address to, uint256 value);
-   
-    //Constructor
     constructor(uint256 total) {
-        
-        addressToBalance[msg.sender] = totalsupply;
+        totalsupply = total;
+        owner = msg.sender;
+        addressToBalance[owner] = totalsupply;
     }
 
     //Public Functions
-    function totalSupply() public override view returns(uint256) {
-        return totalsupply;
-    }
+  
+    function balanceOf(address _owner) public view override returns (uint256 balance){
+		return addressToBalance[_owner];
+	}
 
-    function balanceOf(address tokenOwner) public override view returns(uint256) {
-        return addressToBalance[tokenOwner];
-    }
+	function transfer(address _to, uint256 _value) public virtual override returns (bool success){
+		require(addressToBalance[msg.sender] >= _value, 'Not enough balance in the account');
+		addressToBalance[_to] += _value;
+		addressToBalance[msg.sender] -= _value;
 
-    
-    function transfer(address receiver, uint256 quantity) public override returns(bool) {
-        require(quantity <= addressToBalance[msg.sender], "Insufficient Balance to Transfer");
-        addressToBalance[msg.sender] = addressToBalance[msg.sender] - quantity;
-        addressToBalance[receiver] = addressToBalance[receiver] + quantity;
+		emit Transfer(msg.sender, _to, _value);
+		return true;
+	}
 
-        emit Transfer(msg.sender, receiver, quantity);
-        return true;
-    }
+	function allowance(address _owner, address _spender) public virtual override view returns (uint256 remaining){
+		return allowed[_owner][_spender];
+	}
 
+	function approve(address _spender, uint256 _value) public override returns (bool success){
+		require(balanceOf(msg.sender) >= _value, 'Not enough balance in sender account');
+		require(_value > 0, '0 not allowed');
 
+		allowed[msg.sender][_spender] = _value;
 
+		emit Approval(msg.sender, _spender, _value);
+		return true;
+	}
+
+	function transferFrom(address _from, address _to, uint256 _value) public virtual override returns (bool success){
+		require(balanceOf(_from) >= _value, 'Not enough balance in sender account');
+
+		uint allowedBalance = allowed[_from][msg.sender];
+		require(allowedBalance >= _value, 'Required amount not allowed');
+		addressToBalance[_to] += _value;
+		addressToBalance[_from] -= _value;
+
+		allowed[_from][msg.sender] -= _value;
+
+		emit Transfer(_from, _to, _value);
+		return true;
+	}
 
 }
