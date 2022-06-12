122169f1274439a18c2f2ff87b5adb9dda7b94e7
Saulo Freitas <69553799+SauloFreitas01@users.noreply.github.com>
2022-06-12 19:41:16 -0300

    Update CryptoToken.sol

--- smart_contract/contracts/CryptoToken.sol
+++ smart_contract/contracts/CryptoToken.sol
@@ -5,143 +5,157 @@ import "hardhat/console.sol";
 
 interface IERC20 {
 
-    function totalSupply() external view returns (uint256);
-	function balanceOf(address _owner) external view returns (uint256 balance);
-	function transfer(address _to, uint256 _value) external returns (bool success);
-	function allowance(address _owner, address _spender) external view returns (uint256 remaining);
-	function approve(address _spender, uint256 _value) external returns (bool success);
-	function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
+    // Functions
+    function totalSupply() external view returns(uint256);
+    function balanceOf(address account) external view returns(uint256);
+    function approve(address approved, uint8 quantity) external returns(bool);
+    function allowance(address owner, address approved) external view returns(uint256);
+    function transfer(address receiver, uint256 amount) external returns(bool);
+    function toMint(uint256 amount) external returns(bool);
+    function toBurn(uint256 amount) external returns(bool);
 
-	event Transfer(address indexed _from, address indexed _to, uint256 _value);
-	event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    // Events
+    event Transfer(address from, address to, uint256 value);
+    event Approval(address owner, address approve, uint toSell);
+
 }
-
 contract CryptoToken is IERC20 {
+
+     // Enums
+    enum status { ACTIVE, PAUSED, CANCELLED }
+    
+
+    //Properties
+    string public constant name = "CryptoToken";
+    string public constant symbol = "CRY";
+    uint8 public constant decimals = 9;  //Default dos exemplos é sempre 18
+    uint256 private totalsupply;
+    address public owner;
+    Status contractState;
+
+    mapping(address => mapping(address => uint)) allowed;
+    mapping(address => uint256) private addressToBalance;
 
     modifier isOwner() {
         require(msg.sender == owner , "Sender is not owner!");
         _;
     }
 
-    //Properties
-    string public constant name = "CryptoToken";
-    string public constant symbol = "CRY";
-    uint8 public constant decimals = 3;  //Default dos exemplos é sempre 18
-    uint256 private totalsupply;
-    address public owner;
-
-    enum Status {ACTIVE,PAUSED} 
-    Status contractState;
-
-
-    mapping(address => mapping(address => uint)) allowed;
-    mapping(address => uint256) private addressToBalance;
-
-    
     constructor(uint256 total) {
         totalsupply = total;
         owner = msg.sender;
         addressToBalance[owner] = totalsupply;
-        contractState = Status.PAUSED;
+        contractState = Status.ACTIVE;
 
     }
 
     //Public Functions
-    
-    function setState(uint8 status) public isOwner {
-        if(status == 1){
-            contractState = Status.ACTIVE;
-        }
-        if(status == 2){
-            contractState = Status.PAUSED;
-        }
-        
-    }
-
-    
-    
-    
-    function ownerAddress() public view returns (address){
-        return owner;
-    }
 
     function totalSupply() public override view returns(uint256) {
         return totalsupply;
     }
 
 
-    function balanceOf(address _owner) public view override returns (uint256 balance){
-		return addressToBalance[_owner];
+    function balanceOf(address tokenOwner) public view override returns (uint256 balance){
+		return addressToBalance[tokenOwner];
 	}
 
-	function transfer(address _to, uint256 _value) public virtual override returns (bool success){
-		require(contractState == Status.ACTIVE, "Contrato esta pausado!");
-        require(addressToBalance[msg.sender] >= _value, 'Not enough balance in the account');
-		addressToBalance[_to] += _value;
-		addressToBalance[msg.sender] -= _value;
+    function state() public view returns(status) {
+        return contractState;
+    }
 
-		emit Transfer(msg.sender, _to, _value);
+    function allowance(address tokenOwner, address approved) public virtual override view returns (uint256 remaining){
+		return allowed[tokenOwner][approved];
+	}
+
+    function approve(address approved, uint256 quantity) public override  isOwner returns (bool success){
+		require(contractState == Status.ACTIVE, "Contract paused");
+        require(balanceOf(msg.sender) >= quantity, 'Not enough balance in sender account');
+		require(quantity > 0, 'Value not allowed');
+
+		allowed[msg.sender][approved] = quantity;
+
+		emit Approval(msg.sender, approved, quantity);
 		return true;
 	}
 
-	function allowance(address _owner, address _spender) public virtual override view returns (uint256 remaining){
-		return allowed[_owner][_spender];
-	}
+	function transfer(address receiver, uint256 quantity) public virtual override returns (bool success){
+		require(contractState == Status.ACTIVE, "Contract paused");
+        require(addressToBalance[msg.sender] >= quantity, 'Not enough balance in the account');
+		addressToBalance[receiver] += quantity;
+		addressToBalance[msg.sender] -= quantity;
 
-	function approve(address _spender, uint256 _value) public override returns (bool success){
-		require(contractState == Status.ACTIVE, "Contrato esta pausado!");
-        require(balanceOf(msg.sender) >= _value, 'Not enough balance in sender account');
-		require(_value > 0, '0 not allowed');
-
-		allowed[msg.sender][_spender] = _value;
-
-		emit Approval(msg.sender, _spender, _value);
+		emit Transfer(msg.sender, receiver, quantity);
 		return true;
 	}
 
-	function transferFrom(address _from, address _to, uint256 _value) public virtual override returns (bool success){
-		require(contractState == Status.ACTIVE, "Contrato esta pausado!");
-        require(balanceOf(_from) >= _value, 'Not enough balance in sender account');
+	
+	
 
-		uint allowedBalance = allowed[_from][msg.sender];
-		require(allowedBalance >= _value, 'Required amount not allowed');
-		addressToBalance[_to] += _value;
-		addressToBalance[_from] -= _value;
+	function transferFrom(address sender, address receiver, uint256 quantity) public virtual override returns (bool success){
+		require(contractState == Status.ACTIVE, "Contract paused");
+        require(balanceOf(sender) >= quantity, 'Not enough balance in sender account');
 
-		allowed[_from][msg.sender] -= _value;
+		uint allowedBalance = allowed[sender][msg.sender];
+		require(allowedBalance >= quantity, 'Required amount not allowed');
+		addressToBalance[receiver] += quantity;
+		addressToBalance[sender] -= quantity;
 
-		emit Transfer(_from, _to, _value);
+		allowed[sender][msg.sender] -= quantity;
+
+		emit Transfer(sender, receiver, quantity);
 		return true;
 	}
     
 
-    function mint(address account, uint256 amount) public isOwner {
-        require(contractState == Status.ACTIVE, "Contrato esta pausado!");
+    function mint(uint256 amount) public isOwner returns(bool) {
+        require(contractState == Status.ACTIVE, "Contract paused");
 
-        require(account != address(0), " mint to the zero address");
+        require(amount != 0, "Cannot mint 0 token");
 
         totalsupply += amount;
-        addressToBalance[account] += amount;
-        emit Transfer(address(0), account, amount);       
+        addressToBalance[owner] += amount;
+               
     }
 
 
-    function burn(address account, uint256 amount) public isOwner {
-        require(contractState == Status.ACTIVE, "Contrato esta pausado!");
+    function burn( uint256 amount) public isOwner returns(bool) {
+        require(contractState == Status.ACTIVE, "Contract paused");
 
-        require(account != address(0), " burn from the zero address");
-        uint256 accountBalance = addressToBalance[account];
+        require(amount != 0, "Cannot mint 0 token");
+        uint256 accountBalance = addressToBalance[owner];
         require(accountBalance >= amount, "burn amount exceeds balance");
         
-        addressToBalance[account] = accountBalance - amount;
+        addressToBalance[owner] = accountBalance - amount;
         
         totalsupply -= amount;
 
-        emit Transfer(account, address(0), amount);
+       return true;
 
     } 
+    function changeState(uint8 newState) public isOwner returns(bool) {
 
-    // private function
+        require(newState < 3, "Invalid status option!");
+
+        if (newState == 0) {
+            require(contractState != status.ACTIVE, "The status is already ACTIVE");
+            contractState = status.ACTIVE;
+        } else if (newState == 1) {
+            require(contractState != status.PAUSED, "The status is already PAUSED");
+            contractState = status.PAUSED;
+        } else {
+            require(contractState != status.CANCELLED, "The status is already CANCELLED");
+            contractState = status.CANCELLED;
+        }
+
+        return true;
+    }
+
+    function kill() public isOwner {
+        require(contractState == status.CANCELLED, "It's necessary to cancel the contract before to kill it!");
+        //emit Killed(msg.sender);
+        selfdestruct(payable(owner));
+    }
 
     
 
